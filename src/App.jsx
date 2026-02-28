1afc1ee45ef8c60aa603a54649358cfc64bdbcdf
ärhamchowdhury1218 <arhamchowdhury1218@gmail.com>
2026-02-28 23:40:54 +0600

    implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import logoImg from "./assets/logo.png";
+import currencyLogo from "./assets/Currency-logo.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className="max-w-9/12 mx-auto">
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <img className="w-15 h-15" src={logoImg} alt="" />
+        </div>
+        <div className="flex items-center gap-1">
+          <span className="font-semibold">6000000000</span>
+          <span className="font-semibold">Coin</span>
+          <img className="w-5 h-5" src={currencyLogo} alt="Currency Logo" />
+        </div>
+      </div>
+    </div>
   );
 }
 
